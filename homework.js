44e6b29169329edcbe65e6812a51ef70a0c61275
NastyaPronchuk <56738576+NastyaPronchuk@users.noreply.github.com>
2019-11-13 18:35:23 +0200

    Create homework.js

---
+++ homework.js
@@ -0,0 +1,27 @@
+//https://www.codewars.com/kata/geometry-basics-circle-circumference-in-2d/solutions
+
+const circleCircumference = circle => 2 * Math.PI * circle.radius;
+
+//https://www.codewars.com/kata/training-js-number-13-number-object-and-its-properties/train/javascript
+
+function whatNumberIsIt(n){
+  if(n==Infinity)                        
+    n="Number.POSITIVE_INFINITY";
+  else if(n==1.7976931348623157e+308)    
+    n="Number.MAX_VALUE";
+  else if(n==5e-324)                     
+    n="Number.MIN_VALUE";
+  else if(n==-Infinity)                  
+    n="Number.NEGATIVE_INFINITY";
+  else if(isNaN(n))                      
+    n="Number.NaN";
+ 
+return "Input number is "+n;
+}
+
+//https://www.codewars.com/kata/training-js-number-12-loop-statement-for-dot-in-and-for-dot-of/train/javascript
+??
+
+//https://www.codewars.com/kata/understanding-closures-the-basics/train/javascript
+
+??
